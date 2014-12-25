d905929df63a61d3ceff524c0ce55dcccf538d75
Iain Cambridge <icambridge@inviqa.com>
2014-12-25 15:05:21 +0000

    Remove fatal error in workers

--- goroutines.go
+++ goroutines.go
@@ -6,8 +6,14 @@ import (
 	"sync"
 )
 
+var wg sync.WaitGroup
+
 func worker(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
+		if j == -1 {
+			wg.Done()
+			return
+		}
 		fmt.Println("worker", id, "processing job", j)
 		time.Sleep(time.Second)
 		results <- j * 2
@@ -19,13 +25,11 @@ func printer(results <-chan int) {
 	for r := range results {
 		fmt.Println(r)
 	}
-	
 }
 
 func main() {
 	jobs := make(chan int, 100)
 	results := make(chan int, 100)
-	var wg sync.WaitGroup
 	// This starts up 3 workers, initially blocked
 	// because there are no jobs yet.
 	for w := 1; w <= 3; w++ {
@@ -39,6 +43,8 @@ func main() {
 	for j := 1; j <= 9; j++ {
 		jobs <- j
 	}
+	for w := 1; w <= 3; w++ {
+		jobs <- -1
+	}
 	wg.Wait()
-	close(jobs)
 }
